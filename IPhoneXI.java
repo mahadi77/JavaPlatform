9f84b7f6f8a178f08635f6c770f69534c8eff0a2
Muhammad Hasan <mahadi77@gmail.com>
2020-04-20 21:47:26 -0400

    add new java file

---
+++ IPhoneXI.java
@@ -0,0 +1,18 @@
+public class IPhoneXI{
+
+public static void main(String[] args){
+
+IPhoneXI myIPhoneXI = new IPhoneXI();
+
+myIPhoneXI.call();
+
+}
+
+public void call(){
+
+System.out.println("this method can make a call");
+
+}
+
+}
+
---
+++ Phone.java
@@ -0,0 +1,30 @@
+public class Phone{
+
+public static void main(String[] args){
+
+int numberOfPhone = 10;
+int numberOfFriendPhone = 15;
+int totalPhone = numberOfPhone + numberOfFriendPhone;
+
+System.out.println("total phone to buy" + totalPhone);
+
+char phoneModel = 'y';
+
+System.out.println("model name of phone" + phoneModel);
+
+boolean YouBuyThisPhone = true;
+
+System.out.println("you buy this phone" + YouBuyThisPhone);
+
+String firstName = "Muhammad";
+
+String lastName = " Hasan";
+
+ 
+String fullName = firstName + lastName;
+
+System.out.println("My Name is " + fullName);
+
+}
+
+}
